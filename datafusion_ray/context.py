837c4d9b5afd70aa7cfa7e5f7fe72c7240c91f62
Costi Ciudatu <ccciudatu@gmail.com>
2024-11-06 18:58:10 +0200

    Exclude all DDL statements from Ray scheduling

--- datafusion_ray/context.py
+++ datafusion_ray/context.py
@@ -219,15 +219,11 @@ class DatafusionRayContext:
         self.ctx.register_datalake_table(table_name, paths)
 
     def sql(self, sql: str) -> pa.RecordBatch:
-        # TODO we should parse sql and inspect the plan rather than
-        # perform a string comparison here
-        sql_str = sql.lower()
-        if "create view" in sql_str or "drop view" in sql_str:
-            self.ctx.sql(sql)
-            return []
-
         df = self.df_ctx.sql(sql)
         execution_plan = df.execution_plan()
+        if execution_plan.display().strip() == "EmptyExec":
+            # No ray scheduling for DDL statements
+            return []
 
         graph = self.ctx.plan(execution_plan)
         final_stage_id = graph.get_final_query_stage().id()
